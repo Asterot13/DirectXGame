4591065547acafd5a61eee9d8d6f57c4dc68cd51
DESKTOP-0QMEQ79\WSRP1 <vitaliinovoselov@mail.ru>
2021-08-17 17:59:13 +0600

    add simple graphics engine with initialization of DirectX window

---
+++ SwapChain.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <d3d11.h>
+
+class DeviceContext;
+class SwapChain
+{
+public:
+	SwapChain();
+	// Initialize the swap chain
+	bool Init(HWND hwnd, UINT width, UINT height);
+
+	bool present(bool vsync);
+
+	// Release the swap chain
+	bool Release();
+
+	~SwapChain();
+
+private:
+	IDXGISwapChain* m_swap_chain;
+	ID3D11RenderTargetView* m_rtv;
+private:
+	friend class DeviceContext;
+};
+
